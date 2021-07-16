5e067e0cffc4d64f3abc73424a8f7cce0534b8bf
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-16 09:02:53 -0300

    added permission service

--- src/permissions/service/permissions.service.ts
+++ src/permissions/service/permissions.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from, Observable } from 'rxjs';
+import { Repository } from 'typeorm';
+import { Permission as PermissionEntity } from '../models/permission.entity';
 
 @Injectable()
-export class PermissionsService {}
+export class PermissionsService {
+  constructor(
+    @InjectRepository(PermissionEntity)
+    private readonly permissionRepository: Repository<PermissionEntity>,
+  ) {}
+
+  //  Permissions are readonly in the system
+  
+  findAllPermissions(): Observable<PermissionEntity[]> {
+    return from(this.permissionRepository.find());
+  }
+  findOnePermission(id: number): Observable<PermissionEntity> {
+    return from(this.permissionRepository.findOne(id));
+  }
+}
