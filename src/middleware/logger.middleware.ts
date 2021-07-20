92485d24ed73278cd25f524b774069f3a508a473
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-20 10:43:24 -0300

    updated logger middleware to display full path

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -4,8 +4,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const { method, url } = req;
-    console.log(method + ' ' + url);
+    const { method, baseUrl: pathName } = req;
+    console.log(method + ' ' + pathName);
     next();
   }
 }
