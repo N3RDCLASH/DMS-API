ec515ccede55a67ed064958efe5aed8fb80027f4
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-27 21:59:10 -0300

    Updated Database unique keys

--- db/DMS_FINAL.sql
+++ db/DMS_FINAL.sql
@@ -3,17 +3,13 @@ CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     deleted_at TIMESTAMP NULL
 );
-ALTER TABLE users
-ADD UNIQUE users_username_unique(username);
-ALTER TABLE users
-ADD UNIQUE users_email_unique(email);
 CREATE TABLE documents(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     file VARCHAR(255) NOT NULL,
@@ -50,7 +46,7 @@ CREATE TABLE documents_log(
 );
 CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     deleted_at TIMESTAMP NULL
@@ -65,7 +61,7 @@ CREATE TABLE roles_log(
 );
 CREATE TABLE permissions(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     deleted_at TIMESTAMP NULL
@@ -76,30 +72,32 @@ CREATE TABLE user_has_roles(
     role_id INT NOT NULL
 );
 CREATE TABLE role_has_permissions(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    permission_id INT NOT NULL,
-    role_id INT NOT NULL
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    permission_id INT NOT NULL PRIMARY KEY,
+    role_id INT NOT NULL PRIMARY KEY
 );
 CREATE TABLE document_shared_with_user(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    document_id INT NOT NULL,
-    user_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    document_id INT NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     deleted_at TIMESTAMP NULL
 );
-ALTER TABLE user_has_roles
-ADD CONSTRAINT user_has_roles_role_id_foreign FOREIGN KEY(role_id) REFERENCES roles(id);
-ALTER TABLE user_has_roles
-ADD CONSTRAINT user_has_roles_user_id_foreign FOREIGN KEY(user_id) REFERENCES users(id);
-ALTER TABLE document_shared_with_user
-ADD CONSTRAINT document_shared_with_user_document_id_foreign FOREIGN KEY(document_id) REFERENCES documents(id);
-ALTER TABLE document_shared_with_user
-ADD CONSTRAINT document_shared_with_user_user_id_foreign FOREIGN KEY(user_id) REFERENCES users(id);
-ALTER TABLE role_has_permissions
-ADD CONSTRAINT role_has_permissions_permission_id_foreign FOREIGN KEY(permission_id) REFERENCES permissions(id);
-ALTER TABLE role_has_permissions
-ADD CONSTRAINT role_has_permissions_role_id_foreign FOREIGN KEY(role_id) REFERENCES roles(id);
+ALTER TABLE `user_has_roles`
+ADD CONSTRAINT `user_has_roles_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `roles`(`id`);
+ALTER TABLE `user_has_roles`
+ADD CONSTRAINT `user_has_roles_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE `documents`
+ADD CONSTRAINT `documents_owner_id_foreign` FOREIGN KEY(`owner_id`) REFERENCES `users`(`id`);
+ALTER TABLE `document_shared_with_user`
+ADD CONSTRAINT `document_shared_with_user_document_id_foreign` FOREIGN KEY(`document_id`) REFERENCES `documents`(`id`);
+ALTER TABLE `document_shared_with_user`
+ADD CONSTRAINT `document_shared_with_user_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE `role_has_permissions`
+ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY(`permission_id`) REFERENCES `permissions`(`id`);
+ALTER TABLE `role_has_permissions`
+ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `roles`(`id`);
 -- documents trigger ;
 CREATE TRIGGER documents_log_BEFORE_UPDATE BEFORE
 UPDATE ON documents FOR EACH ROW
@@ -130,11 +128,10 @@ SET user_id = NEW.id,
     updated_by = NEW.id;
 -- roles log trigger;
 CREATE TRIGGER roles_log_BEFORE_UPDATE BEFORE
-UPDATE ON roles FOR EACH ROW
+UPDATE ON ROLES FOR EACH ROW
 INSERT INTO roles_log
 SET role_id = NEW.id,
     name_old = OLD.name,
     name_new = NEW.name,
     updated_at = CURRENT_TIMESTAMP;
-
 -- Insert Default Permissions and roles
