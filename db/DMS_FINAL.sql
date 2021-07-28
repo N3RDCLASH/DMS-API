ed94c3e584b93e37c0918f5a6b4523a4efe9640e
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-28 01:46:38 -0300

    Added basic permissions and  roles to db

--- db/DMS_FINAL.sql
+++ db/DMS_FINAL.sql
@@ -137,3 +137,36 @@ SET role_id = NEW.id,
     name_new = NEW.name,
     updated_at = CURRENT_TIMESTAMP;
 -- Insert Default Permissions and roles
+INSERT INTO `permissions` (`name`)
+VALUES ('CREATE_USER');
+INSERT INTO `permissions` (`name`)
+VALUES ('READ_USER');
+INSERT INTO `permissions` (`name`)
+VALUES ('UPDATE_USER');
+INSERT INTO `permissions` (`name`)
+VALUES ('DELETE_USER');
+-- 
+INSERT INTO `permissions` (`name`)
+VALUES ('CREATE_DOCUMENTS');
+INSERT INTO `permissions` (`name`)
+VALUES ('READ_DOCUMENTS');
+INSERT INTO `permissions` (`name`)
+VALUES ('UPDATE_DOCUMENTS');
+INSERT INTO `permissions` (`name`)
+VALUES ('DELETE_DOCUMENTS');
+INSERT INTO `permissions` (`name`)
+VALUES ('SHARE_DOCUMENTS');
+-- 
+INSERT INTO `permissions` (`name`)
+VALUES ('CREATE_ROLES');
+INSERT INTO `permissions` (`name`)
+VALUES ('READ_ROLES');
+INSERT INTO `permissions` (`name`)
+VALUES ('UPDATE_ROLES');
+INSERT INTO `permissions` (`name`)
+VALUES ('DELETE_ROLES');
+-- 
+INSERT INTO `roles` (`name`)
+VALUES ('admin');
+INSERT INTO `roles` (`name`)
+VALUES ('user');
