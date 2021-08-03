64435bbaca7bfd6e673e95c75ed6c4d11eed9303
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-03 18:30:27 -0300

    updated database for submission

--- db/DMS_FINAL.sql
+++ db/DMS_FINAL.sql
@@ -1,4 +1,3 @@
-USE test_dms;
 CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     firstname TEXT NOT NULL,
@@ -71,8 +70,8 @@ CREATE TABLE user_has_roles(
     role_id INT NOT NULL
 );
 CREATE TABLE role_has_permissions(
-    permission_id INT NOT NULL,
     role_id INT NOT NULL,
+    permission_id INT NOT NULL,
     PRIMARY KEY(permission_id, role_id)
 );
 CREATE TABLE document_shared_with_user(
@@ -163,8 +162,83 @@ INSERT INTO `permissions` (`name`)
 VALUES ('UPDATE_ROLES');
 INSERT INTO `permissions` (`name`)
 VALUES ('DELETE_ROLES');
+-- admin@DMS.com-DMS@dmin
+INSERT INTO `users` (
+        `id`,
+        `firstname`,
+        `lastname`,
+        `username`,
+        `email`,
+        `password`
+    )
+VALUES (
+        1,
+        'Joel',
+        'Naarendorp',
+        'admin',
+        'admin@DMS.com',
+        '$2y$12$B4QKeWtd38J.jXZEEAh3.OJRpzZx9.bYQIpoz9dSlisSGbWolsaT6'
+    );
+INSERT INTO `users` (
+        `id`,
+        `firstname`,
+        `lastname`,
+        `username`,
+        `email`,
+        `password`
+    )
+VALUES (
+        2,
+        'Joel',
+        'Naarendorp',
+        'user',
+        'user@DMS.com',
+        '$2y$12$3fBt1Ud / gxSkYW0siThmA.yeT4ECLga2JNARaEvDuBuOEVUnedvWO'
+    );
 -- 
 INSERT INTO `roles` (`name`)
 VALUES ('admin');
 INSERT INTO `roles` (`name`)
 VALUES ('user');
+-- 
+INSERT INTO `user_has_roles` (`user_id`, `role_id`)
+VALUES (1, 1);
+INSERT INTO `user_has_roles` (`user_id`, `role_id`)
+VALUES (2, 2);
+-- 
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 1);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 2);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 3);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 4);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 5);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 6);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 7);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 8);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 9);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 10);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 11);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 12);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (1, 13);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (2, 5);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (2, 6);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (2, 7);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (2, 8);
+INSERT INTO `role_has_permissions` (`role_id`, `permission_id`)
+VALUES (2, 9);
