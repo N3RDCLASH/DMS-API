d34f67b25b31a914d83bd13b8848786807fcf628
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-19 21:11:04 -0300

    added auditing base to documents

--- src/documents/models/document.interface.ts
+++ src/documents/models/document.interface.ts
@@ -1,15 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { AuditingBase } from 'src/auditingbase/auditingbase.interface';
 import { User } from 'src/users/models/user.entity';
 import { Timestamp } from 'typeorm';
 
-export interface Document {
+export interface Document extends AuditingBase {
   file: string;
   owner_id: number;
-  created_at?: Timestamp;
-
-  updated_at?: Timestamp;
-
-  deleted_at?: Timestamp;
-
   shared_users?: User[];
 }
