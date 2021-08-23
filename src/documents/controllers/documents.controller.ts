d7087cea1d45ea7025e73c79d7568eeadb23b2e8
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-23 11:05:11 -0300

    removed unused imports and refactored getAllDocuments

--- src/documents/controllers/documents.controller.ts
+++ src/documents/controllers/documents.controller.ts
@@ -3,14 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  Header,
   Param,
   Post,
-  Query,
   Req,
   Request,
   Res,
-  StreamableFile,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -22,16 +19,12 @@ import {
   uploadOptions,
 } from 'src/middleware/fileupload.utils';
 import { DocumentBuilder } from '../document.builder';
-import { UserBuilder } from 'src/users/user.builder';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ShareDocumentDto, UploadFileDto } from '../models/document.dto';
 import { JwtAuthGuard } from 'src/auth/gaurds/jwt-auth.gaurd';
-import { RolesGuard } from 'src/auth/gaurds/roles.guard';
 import { join } from 'path';
 import { map } from 'rxjs/operators';
-import * as fs from 'fs';
 import * as escape from 'escape-path-with-spaces';
-import { of } from 'rxjs';
 @ApiTags('documents')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
@@ -58,30 +51,33 @@ export class DocumentsController {
   }
 
   @Get()
-  getAllDocumentsByUser(@Query('user_id') owner_id: number) {
-    return this.documentService.findAllDocumentsByUser(owner_id);
+  getAllDocumentsByUser(@Req() req: Request) {
+    const {
+      user: { id },
+    } = getUserFromRequestToken(req);
+    return this.documentService.findAllDocumentsByUser(id);
   }
+
   @Get(':id')
   getOneDocument(@Param('id') id: number) {
     return this.documentService.findSingleDocument(id);
   }
+
   @Get(':id/download')
   downloadOneDocument(@Param('id') id: number, @Res() res) {
-    console.log('document');
     return this.documentService.findSingleDocument(id).pipe(
       map(async (document) => {
-        // const file = createReadStream(join(process.cwd(), document.file));
         res.header('X-Suggested-Filename', document.file.split('\\')[1]);
         res.download(join(process.cwd(), escape(document.file)));
       }),
     );
   }
+
   @Delete(':id')
   deleteOneDocument(@Param('id') id: number) {
     return this.documentService.deleteOneDocument(id);
   }
 
-  // todo: add documentshare fucntionality to controller
   @Post(':id/share')
   shareDocument(
     @Param('id') document_id: number,
