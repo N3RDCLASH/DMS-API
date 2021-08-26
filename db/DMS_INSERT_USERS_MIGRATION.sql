62314c10f5ac3b82cc99190ae8449853b08ea4df
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-26 12:41:21 -0300

    Split database to into different scripts

---
+++ db/DMS_INSERT_USERS_MIGRATION.sql
@@ -0,0 +1,33 @@
+-- admin@DMS.com-DMS@dmin
+INSERT INTO `users` (
+        `id`,
+        `firstname`,
+        `lastname`,
+        `username`,
+        `email`,
+        `password`
+    )
+VALUES (
+        1,
+        'Joel',
+        'Naarendorp',
+        'admin',
+        'admin@DMS.com',
+        '$2y$12$B4QKeWtd38J.jXZEEAh3.OJRpzZx9.bYQIpoz9dSlisSGbWolsaT6'
+    );
+INSERT INTO `users` (
+        `id`,
+        `firstname`,
+        `lastname`,
+        `username`,
+        `email`,
+        `password`
+    )
+VALUES (
+        2,
+        'Joel',
+        'Naarendorp',
+        'user',
+        'user@DMS.com',
+        '$2y$12$3fBt1Ud.gxSkYW0siThmA.yeT4ECLga2JNARaEvDuBuOEVUnedvWO'
+    );
