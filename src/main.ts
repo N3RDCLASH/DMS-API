0f6272f4b995e29ed80d860530fa06b6da7f8dff
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-09-02 08:54:04 -0300

    added spacing=

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); 
 
   // enable cors for API
   app.enableCors({ exposedHeaders: [' X-Suggested-Filename'] });
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
